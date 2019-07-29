cc035976738d3e1ccaf23aa0d7ba9aad0d095d23
黄麒二 <huangqier@hogecloud.com>
2019-07-29 11:50:10 +0800

    增加测试tab

--- src/router.ts
+++ src/router.ts
@@ -21,6 +21,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/test",
+      name: "test",
+      component: () => import("./views/test.vue")
     }
   ]
 });
---
+++ src/views/test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="test">
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      message: 123,
+    }
+  },
+  methods: {
+   
+  },
+  components: {
+   
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
