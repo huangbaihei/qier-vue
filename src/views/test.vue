4d28c88d0a2e08b2dbfef902fdc0eca8cac0527c
黄麒二 <huangqier@hogecloud.com>
2019-08-12 14:50:47 +0800

    增加vant库中的轮播图

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="test">
     <h1>{{message}}</h1>
+    <van-swipe :autoplay="3000" :height="200" indicator-color="white">
+      <van-swipe-item><div class="item">123</div></van-swipe-item>
+      <van-swipe-item><div class="item">fds</div></van-swipe-item>
+    </van-swipe>
   </div>
 </template>
 
 <script>
+import { Swipe, SwipeItem } from 'vant';
+import 'vant/lib/swipe/index.css';
+import 'vant/lib/swipe-item/index.css';
 
 export default {
   data() {
@@ -16,12 +23,18 @@ export default {
    
   },
   components: {
-   
+    vanSwipe: Swipe,
+    vanSwipeItem: SwipeItem
   }
 }
 </script>
 
 <style lang="scss">
-
+  .van-swipe-item:nth-child(even) {
+    background-color: #39a9ed;
+  }
+  .van-swipe-item:nth-child(odd) {
+    background-color: red;
+  }
 </style>
 
